8d4feb4497f5f001711d6530a132ededc9298418
FEd0rA-S <rdxdcsgo@gmail.com>
2024-11-19 13:41:32 +0300

    Add files via upload

---
+++ Window2.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Практика2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Answer_Click(object sender, RoutedEventArgs e)
+        {
+            string result;
+            double first;
+            double second;
+            double answer;
+            string op = Operator.Text;
+            switch (op)
+            {
+                case "+":
+                    first = Convert.ToDouble(NumberA.Text);
+                    second = Convert.ToDouble(NumberB.Text);
+                    answer = first + second;
+                    result = Convert.ToString(answer);
+                    ResultL.Content = result;
+                    break;
+                case "-":
+                    first = Convert.ToDouble(NumberA.Text);
+                    second = Convert.ToDouble(NumberB.Text);
+                    answer = first - second;
+                    result = Convert.ToString(answer);
+                    ResultL.Content = result;
+                    break;
+                case "*":
+                    first = Convert.ToDouble(NumberA.Text);
+                    second = Convert.ToDouble(NumberB.Text);
+                    answer = first * second;
+                    result = Convert.ToString(answer);
+                    ResultL.Content = result;
+                    break;
+                case "/":
+                    first = Convert.ToDouble(NumberA.Text);
+                    second = Convert.ToDouble(NumberB.Text);
+                    if (second == 0)
+                    {
+                        ResultL.Content = "На 0 делить нельзя";
+                    }
+                    else
+                    {
+                        answer = first / second;
+                        result = Convert.ToString(answer);
+                        ResultL.Content = result;
+                    }
+                    break;
+                default:
+                    ResultL.Content = "Ошибка";
+                    break;
+            }
+            
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow pw = new MainWindow();
+            pw.Show();
+            Close();
+        }
+    }
+}
---
+++ Window4.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Практика2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window4.xaml
+    /// </summary>
+    public partial class Window4 : Window
+    {
+        public Window4()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string password1 = Convert.ToString(NumberX.Text);
+            string password = "пароль";
+            if (password1 == "пароль") Resultat.Content = "Пароль верный. \n Секретное сообщение.";
+            else if (password1 != password) Resultat.Content = "Пароль неверный, попробуйте ещё.";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow pw = new MainWindow();
+            pw.Show();
+            Close();
+        }
+
+        private void NumberX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
